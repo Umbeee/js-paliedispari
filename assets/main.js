a3d91868bcf8e37c5b8be50a701f538348ed973f
Umbeee <umberto.nesta@hotmail.com>
2023-04-03 09:28:01 +0200

    completato esercizio

--- assets/main.js
+++ assets/main.js
@@ -28,3 +28,60 @@ function parola(){
     }
 
 }  
+
+function numero(){
+    let inputNumero = document.getElementById('inputNumero').value;
+    inputNumero = parseInt(inputNumero);
+    console.log(inputNumero);
+
+    let pariDispari = document.getElementById('pariDispari').value;
+    console.log(pariDispari);
+
+
+    let numeroRandom = 0;
+
+    for(let i=0; i< 1; i++){
+        let random = Math.floor(Math.random() * 10);
+        if(random <= 5 && random>0){
+            numeroRandom = random;
+        }else{
+            i= -1;
+        }
+    }
+
+    console.log(numeroRandom)
+
+
+    let stampaNumero = document.getElementById('stampaNumero');
+    var somma = numeroRandom + inputNumero;
+
+    let soldatino = false;
+
+
+    if (pariDispari == 'pari'){
+        console.log('ha scelto pari')
+    }else{
+        console.log('ha scelto dispari')
+    }
+
+
+
+     if(somma % 2 == 0) //se la somma è pari, allora : 
+        {
+            if(pariDispari == 'pari'){ //se quello che ha scelto l'utente è pari
+                stampaNumero.innerHTML += `<p> Il numero scelto dall'utente è: <span>${inputNumero}</span>. Il numero generato randomicamente è: <span>${numeroRandom}</span>. La somma è <span>${somma}</span> ed è pari, l'utente ha <span>VINTO!</span> </p>`
+            }else{
+                stampaNumero.innerHTML += `<p> Il numero scelto dall'utente è: <span>${inputNumero}</span>. Il numero generato randomicamente è: <span>${numeroRandom}</span>. La somma è <span>${somma}</span> ed è pari, l'utente ha <span>PERSO!</span> </p>`
+            }
+        }
+    else{
+        if(pariDispari == 'pari'){ //se quello che ha scelto l'utente è pari
+            stampaNumero.innerHTML += `<p> Il numero scelto dall'utente è: <span>${inputNumero}</span>. Il numero generato randomicamente è: <span>${numeroRandom}</span>. La somma è <span>${somma}</span> ed è pari, l'utente ha <span>PERSO!</span> </p>`
+        }else{
+            stampaNumero.innerHTML += `<p> Il numero scelto dall'utente è: <span>${inputNumero}</span>. Il numero generato randomicamente è: <span>${numeroRandom}</span>. La somma è <span>${somma}</span> ed è pari, l'utente ha <span>VINTO!</span> </p>`
+        }
+    }
+
+    
+
+}
